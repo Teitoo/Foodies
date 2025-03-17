b7d7e7a520a1071665562f94d149ef0585468c5a
Teitoo <aop2999@gmail.com>
2025-03-17 18:38:25 +0800

    UserController部分新增了幾個API，新增UserService內的updateUser

--- src/main/java/com/sideproject/foodies/controller/UserController.java
+++ src/main/java/com/sideproject/foodies/controller/UserController.java
@@ -1,5 +1,34 @@
 package com.sideproject.foodies.controller;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sideproject.foodies.beans.Cuisine;
+import com.sideproject.foodies.beans.Order;
+import com.sideproject.foodies.service.CuisineService;
+import com.sideproject.foodies.service.OrderService;
+
+@RestController
+public class UserController {
+	@Autowired
+	private CuisineService cuisineService;
+	
+	@Autowired
+	private OrderService orderService;
+	
+	@GetMapping("/menu")
+	public List<Cuisine> getMenu() {
+		return cuisineService.findAll();
+	}
+	
+	@PostMapping("/{userId}/order")
+	public Order placeOrder(@PathVariable Long id, @RequestBody List<Long> cuisineIds) {
+		return orderService.placeOrder(id, cuisineIds);
+	}
 }
--- src/main/java/com/sideproject/foodies/service/UserService.java
+++ src/main/java/com/sideproject/foodies/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sideproject.foodies.beans.Order;
 import com.sideproject.foodies.beans.User;
 import com.sideproject.foodies.exception.EntityNotFoundException;
 import com.sideproject.foodies.repository.UserRepository;
@@ -13,6 +14,11 @@ import com.sideproject.foodies.repository.UserRepository;
 public class UserService {
 	@Autowired
 	UserRepository uRepo;
+	
+	
+	public User createUser(User u) {
+		return uRepo.save(u);
+	}
 	
 	public List<User> findAll() {
 		return uRepo.findAll();
@@ -23,13 +29,30 @@ public class UserService {
 				.orElseThrow(() -> new EntityNotFoundException("User not found with id: " + id));
 	}
 	
-	public User createUser(User u) {
+	public User updateUser(Long id, User u) {
+		User oldUser = uRepo.findById(id)
+				.map(existingUser -> {
+					existingUser.setAccount(u.getAccount());
+					existingUser.setPassword(u.getPassword());
+					existingUser.setUserName(u.getUserName());
+					existingUser.setAddress(u.getAddress());
+					existingUser.setEmail(u.getEmail());
+					existingUser.setPhoneNumber(u.getPhoneNumber());
+					return existingUser;
+				})
+				.orElseThrow(() -> new EntityNotFoundException("User not found with id: " + id));
+		//清除User和Order的原有關聯後新增
+		oldUser.getOrders().forEach(order -> order.setUser(null));
+		for(Order o : u.getOrders()) {
+			o.setUser(u);
+			u.getOrders().add(o);
+		}
 		return uRepo.save(u);
 	}
-	
 	public void deleteById(Long id) {
 		User u = uRepo.findById(id)
 				.orElseThrow(() -> new EntityNotFoundException("User not found with id: "+ id));
 		uRepo.delete(u);
 	}
+	
 }
