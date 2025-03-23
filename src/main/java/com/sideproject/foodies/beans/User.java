5e486a1854bcb5cdb310384bfd94f5b6d5c90fd8
Teitoo <aop2999@gmail.com>
2025-03-23 15:26:39 +0800

    User類實作UserDetails

--- src/main/java/com/sideproject/foodies/beans/User.java
+++ src/main/java/com/sideproject/foodies/beans/User.java
@@ -1,10 +1,17 @@
 package com.sideproject.foodies.beans;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,21 +27,41 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name="Users")
-public class User {
+public class User implements UserDetails{
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
 	@NotNull
-	private String account;
+	private String username;
 	@NotNull
 	private String password;
 	
 //	user data：
-	private String userName;
 	private String email;
 	private String phoneNumber;
 	private String address;
+	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	
 	@OneToMany(cascade = 
 		{CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "user")
 	private List<Order> orders = new ArrayList<>();
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+
+	@Override
+	public String getUsername() { return username; }
+	
+	@Override
+	public String getPassword() { return password; }
 }
