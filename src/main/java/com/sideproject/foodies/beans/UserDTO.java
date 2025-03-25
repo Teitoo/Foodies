d6c2992865ae5efdb5a9defcd2e58bef7c66265f
Teitoo <aop2999@gmail.com>
2025-03-25 13:58:34 +0800

    UserDTO加上List<Order>欄位

--- src/main/java/com/sideproject/foodies/beans/UserDTO.java
+++ src/main/java/com/sideproject/foodies/beans/UserDTO.java
@@ -1,5 +1,7 @@
 package com.sideproject.foodies.beans;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,10 +11,21 @@ public class UserDTO {
 	private String username;
 	@Getter
 	private String email;
-//	@Getter
-//	private String phoneNumber;
-//	@Getter
-//	private String address;
+	@Getter
+	private String phoneNumber;
+	@Getter
+	private String address;
 	@Getter
 	private Role role;
+	@Getter
+	private List<Order> orders;
+	
+	public UserDTO(User user) {
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.phoneNumber = user.getPhoneNumber();
+        this.address = user.getAddress();
+        this.role = user.getRole();
+        this.orders = user.getOrders();
+    }
 }
